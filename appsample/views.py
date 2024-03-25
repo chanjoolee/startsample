59fa6d037c448b6bd3b5beae35a095e2e7f3a78a
chanjoolee <gregrechan@gmail.com>
2024-03-25 15:44:21 +0900

    get and post

--- appsample/views.py
+++ appsample/views.py
@@ -7,7 +7,10 @@ from rest_framework import generics
 from .models import Item
 from .serializers import ItemSerializer
 from django.db import connection
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+
 
 class ItemListCreateView(generics.ListCreateAPIView):
     queryset = Item.objects.all()
@@ -25,9 +28,27 @@ def execute_raw_sql(request):
         results = [ dict(zip(columns,row)) for row in cursor.fetchall()]
     return JsonResponse({"data": results})
 
+@csrf_exempt
 def get_item_list(request):
+
+    # Determine the request method and get the appropriate parameter
+    if request.method == 'GET':
+        search_name = request.GET.get('name', '')
+    elif request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            search_name = data.get('name', '')
+        except json.JSONDecodeError:
+            return HttpResponse("Invalid JSON", status=400)
+        
+
     with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM appsample_item")
+        sql_template = f"""
+        SELECT * FROM appsample_item
+        WHERE name like concat('%' ,'{search_name}' , '%')
+        """
+        params = {"name":  search_name}
+        cursor.execute(sql_template)
         columns = [snake_to_camel(col[0]) for col in cursor.description]
         results = [ dict(zip(columns,row)) for row in cursor.fetchall()]
     return JsonResponse({"data": results})
